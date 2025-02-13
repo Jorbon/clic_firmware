8425c72159f1a040b591d472a90b99ed2737fe1e
Jorbon <ben.a.phillips@outlook.com>
2025-02-13 12:38:23 -0600

    function reorg

--- main.c
+++ main.c
@@ -4,54 +4,83 @@
 #include <stdlib.h>
 
 
+
+
 void error_callback(int error, const char* description) {
 	fprintf(stderr, "GLFW Error: %s\n", description);
 }
 
 
-GLuint load_generic_image(const char* image_path) {
+typedef struct {
+	unsigned char* data;
+	unsigned int width;
+	unsigned int height;
+	unsigned int channels;
+} Image;
+
+typedef struct {
+	GLuint font_texture;
+} Font;
+
+
+Image load_png(const char* path) {
+	Image img;
 	
-	FILE* fp = fopen(image_path, "rb");
-	if (!fp) {
-		printf("File not found: %s\n", image_path);
-		return 0;
+	FILE* file_pointer = fopen(path, "rb");
+	if (!file_pointer) {
+		printf("File not found: %s\n", path);
+		return img;
 	}
 	
 	unsigned char sig[8];
-	fread(sig, 1, 8, fp);
+	fread(sig, 1, 8, file_pointer);
 	if (!png_check_sig(sig, 8)) {
-		printf("File at '%s' is not a PNG.\n", image_path);
-		return 0;
+		printf("File at '%s' is not a PNG.\n", path);
+		return img;
 	}
 	
 	png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
 	png_infop info_ptr = png_create_info_struct(png_ptr);
 	
-	png_init_io(png_ptr, fp);
+	png_init_io(png_ptr, file_pointer);
 	png_set_sig_bytes(png_ptr, 8);
 	png_read_info(png_ptr, info_ptr);
 	
-	int width, height, bit_depth, color_type;
-	png_get_IHDR(png_ptr, info_ptr, &width, &height, &bit_depth, &color_type, NULL, NULL, NULL);
+	int bit_depth, color_type;
+	png_get_IHDR(png_ptr, info_ptr, &img.width, &img.height, &bit_depth, &color_type, NULL, NULL, NULL);
+	if (bit_depth == 16) png_set_strip_16(png_ptr);
+	if (color_type == PNG_COLOR_TYPE_PALETTE) png_set_palette_to_rgb(png_ptr);
+	if (color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8) png_set_expand_gray_1_2_4_to_8(png_ptr);
+	if (png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS)) png_set_tRNS_to_alpha(png_ptr);
+	if (color_type == PNG_COLOR_TYPE_RGB || color_type == PNG_COLOR_TYPE_GRAY || color_type == PNG_COLOR_TYPE_PALETTE) png_set_filler(png_ptr, 255, PNG_FILLER_AFTER);
 	if (color_type == PNG_COLOR_TYPE_GRAY || color_type == PNG_COLOR_TYPE_GRAY_ALPHA) png_set_gray_to_rgb(png_ptr);
 	//png_set_gamma(png_ptr, display_exponent, gamma);
-	
-	
 	png_read_update_info(png_ptr, info_ptr);
+
 	unsigned int bytes_per_row = png_get_rowbytes(png_ptr, info_ptr);
-	int channels = (int) png_get_channels(png_ptr, info_ptr);
+	img.channels = png_get_channels(png_ptr, info_ptr);
 	
-	unsigned char* image_data = (unsigned char*) malloc(bytes_per_row * height);
-	if (!image_data) {
+	img.data = (unsigned char*) malloc(bytes_per_row * img.height);
+	if (!img.data) {
 		printf("Not enough memory for image buffer.\n");
-		return 0;
+		return img;
 	}
 	
-	unsigned char* row_pointers[height];
-	for (int i = 0; i < height; i++) row_pointers[i] = image_data + i * bytes_per_row;
+	unsigned char* row_pointers[img.height];
+	for (int i = 0; i < img.height; i++) row_pointers[i] = img.data + i * bytes_per_row;
 	png_read_image(png_ptr, row_pointers);
 	png_read_end(png_ptr, NULL);
 	
+	png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+	fclose(file_pointer);
+
+	return img;
+}
+
+
+GLuint load_generic_image(const char* path) {
+	
+	Image img = load_png(path);
 	
 	GLuint texture;
 	glGenTextures(1, &texture);
@@ -62,17 +91,34 @@ GLuint load_generic_image(const char* image_path) {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img.width, img.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img.data);
 	
-	
-	png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-	fclose(fp);
-	free(image_data);
+	free(img.data);
 	
 	return texture;
 }
 
 
+GLuint load_font(const char* path) {
+	
+	Image img = load_png(path);
+	
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img.width, img.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img.data);
+	
+	
+	free(img.data);
+	
+	return texture;
+}
 
 
 
@@ -104,7 +150,7 @@ int main() {
 	
 	
 	
-	GLuint test_tex = load_generic_image("/root/clic_firmware/assets/mc.png");
+	GLuint test_tex = load_font("/root/clic_firmware/assets/consolas16.png");
 	if (!test_tex) return 1;
 	
 	
