801f3c5dd0949d991bfaa2aa2418ae2cc80edc2e
Jorbon <ben.a.phillips@outlook.com>
2025-02-13 14:10:29 -0600

    text rendering working

--- main.c
+++ main.c
@@ -18,9 +18,15 @@ typedef struct {
 	unsigned int channels;
 } Image;
 
-typedef struct {
-	GLuint font_texture;
-} Font;
+
+static const unsigned int display_width = 1920;
+static const unsigned int display_height = 1080;
+static const float widthf = (float) display_width;
+static const float heightf = (float) display_height;
+static const float widthf_inv = 1.0 / widthf;
+static const float heightf_inv = 1.0 / heightf;
+static const float aratio = widthf * heightf_inv;
+static const float aratio_inv = heightf * widthf_inv;
 
 
 Image load_png(const char* path) {
@@ -78,10 +84,7 @@ Image load_png(const char* path) {
 }
 
 
-GLuint load_generic_image(const char* path) {
-	
-	Image img = load_png(path);
-	
+GLuint init_texture(Image img) {
 	GLuint texture;
 	glGenTextures(1, &texture);
 	glBindTexture(GL_TEXTURE_2D, texture);
@@ -92,7 +95,14 @@ GLuint load_generic_image(const char* path) {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img.width, img.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img.data);
-	
+
+	return texture;
+}
+
+
+GLuint load_generic_image(const char* path) {
+	Image img = load_png(path);
+	GLuint texture = init_texture(img);
 	free(img.data);
 	
 	return texture;
@@ -100,26 +110,64 @@ GLuint load_generic_image(const char* path) {
 
 
 GLuint load_font(const char* path) {
-	
 	Image img = load_png(path);
 	
-	GLuint texture;
-	glGenTextures(1, &texture);
-	glBindTexture(GL_TEXTURE_2D, texture);
-	
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img.width, img.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img.data);
 	
 	
+	
+	GLuint texture = init_texture(img);
 	free(img.data);
 	
 	return texture;
 }
 
+
+
+void draw_quad(float x, float y, float width, float height, float u_min, float u_max, float v_min, float v_max) {
+	float x1 = x * 2.0 - 1.0;
+	float x2 = (x + width) * 2.0 - 1.0;
+	float y1 = 1.0 - y * 2.0;
+	float y2 = 1.0 - (y + height) * 2.0;
+	glTexCoord2f(u_min, v_max); glVertex3f(x1, y2, 0.0f);
+	glTexCoord2f(u_max, v_max); glVertex3f(x2, y2, 0.0f);
+	glTexCoord2f(u_max, v_min); glVertex3f(x2, y1, 0.0f);
+	glTexCoord2f(u_min, v_min); glVertex3f(x1, y1, 0.0f);
+}
+
+void draw_image(GLuint texture, float x, float y, float width, float height) {
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glBegin(GL_QUADS);
+	draw_quad(x, y, width, height, 0.0, 1.0, 0.0, 1.0);
+	glEnd();
+}
+
+void draw_text(char* text, GLuint font, float x, float y) {
+	unsigned int char_size = 16;
+	unsigned int char_width = 9;
+	float char_sizef = (float) char_size;
+	float char_widthf = (float) char_width;
+	float char_sizef_inv = 1.0 / char_sizef;
+	float char_widthf_inv = 1.0 / char_widthf;
+	
+	glBindTexture(GL_TEXTURE_2D, font);
+	glBegin(GL_QUADS);
+	
+	char c;
+	while (1) {
+		c = *text;
+		if (c == 0) break;
+		
+		float u = ((float) (c & 0b1111) + (1.0 - char_widthf * char_sizef_inv) * 0.5) * 0.0625;
+		float v = (float) ((c >> 4) & 0b111) * 0.125;
+		
+		draw_quad(x, y, char_widthf * widthf_inv, char_sizef * heightf_inv, u, u + char_widthf * char_sizef_inv * 0.0625, v, v + 0.125);
+		x += char_widthf * widthf_inv;
+		
+		text += 1;
+	}
+	
+	glEnd();
+}
 
 
 
@@ -132,9 +180,6 @@ int main() {
 		return 1;
 	}
 	
-	int display_width = 1920;
-	int display_height = 1080;
-	
 	GLFWwindow* window = glfwCreateWindow(display_width, display_height, "super cool window name", NULL, NULL);
 	if (!window) {
 		glfwTerminate();
@@ -142,18 +187,18 @@ int main() {
 		return 1;
 	}
 	
-	printf("window exists!\n");
-	
 	glfwMakeContextCurrent(window);
 	glEnable(GL_TEXTURE_2D);
 	
 	
 	
 	
-	GLuint test_tex = load_font("/root/clic_firmware/assets/consolas16.png");
+	GLuint test_tex = load_generic_image("/root/clic_firmware/assets/mc.png");
 	if (!test_tex) return 1;
 	
-	
+	GLuint font = load_font("/root/clic_firmware/assets/consolas16.png");
+	if (!font) return 1;
+
 	
 	glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
 	
@@ -163,13 +208,9 @@ int main() {
 		
 		glClear(GL_COLOR_BUFFER_BIT);
 		
-		glBindTexture(GL_TEXTURE_2D, test_tex);
-		glBegin(GL_QUADS);
-		glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, -1.0f, 0.0f);
-		glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f, -1.0f, 0.0f);
-		glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f,  1.0f, 0.0f);
-		glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f,  1.0f, 0.0f);
-		glEnd();
+		draw_image(test_tex, 0.2, 0.2, 0.6, 0.6);
+		draw_text("Hi test text for rendering (%#*!^&(%)^#@*(&'~`_--+=,<>./?:;[]{}\\|", font, 0.1, 0.1);
+		
 		
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -179,7 +220,8 @@ int main() {
 	
 	
 	
-	glfwDestroyWindow(window); printf("destroy did not segfault\n");
-	glfwTerminate(); printf("terminate did not segfault\n");
+	glfwDestroyWindow(window);
+	printf("\nvvv glfwTerminate about to segfault lol\n");
+	glfwTerminate();
 	return 0;
 }
